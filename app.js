505ab5abb4e794c151da0468c3c8966f29889d00
Divyansh Mahavar <79765685+divyanshkm@users.noreply.github.com>
2025-02-28 12:49:20 +0530

    create express server

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('Hello World 3000');
+import express from "express";
+
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.send('Welcome to the Subscription Tracker API');
+})
+
+app.listen(3000,()=>{
+    console.log('Server is running on port 3000');
+})
